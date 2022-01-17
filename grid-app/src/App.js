93f4f51dbaa961965488ce6598cb6fba0bd070ea
Awais <razhar5214@bths.edu>
2022-01-17 18:13:31 -0500

    closes issue #2 addRow and addCol work

--- grid-app/src/App.js
+++ grid-app/src/App.js
@@ -12,13 +12,14 @@ export default function App() {
       cols:0
     }
   );
-
+   
   function addRow(){
     //console.log('add row');
     setGrid(prevGrid => ({
       ...prevGrid,
       rows: prevGrid.rows+1
     }))
+    appendRow(grid.rows);
   }
 
   function addColumn(){
@@ -27,9 +28,36 @@ export default function App() {
       ...prevGrid,
       cols: prevGrid.cols+1
     }))
+    appendColumn();
+  }
+  function appendRow(rows) {
+    var tbl = document.getElementById('my-table'), // table reference
+        row = tbl.insertRow(tbl.rows.length),      // append table row
+        i;
+    // insert table cells to the new row
+    for (i = 0; i < tbl.rows[0].cells.length; i++) {
+        createCell(row.insertCell(i));
+    }
+  }
+  function appendColumn() {
+    var tbl = document.getElementById('my-table'), // table reference
+        i;
+    // open loop for each row and append cell
+    for (i = 0; i < tbl.rows.length; i++) {
+        createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length));
+    }
+}
+ 
+// create DIV element and append to the table cell
+  function createCell(cell, style) {
+      var div = document.createElement('div'); // create DIV element
+      div.setAttribute('class', style);        // set DIV class attribute
+      div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
+      cell.appendChild(div);                   // append DIV to the table cell
   }
 
 
+ 
     return (
       <div className="App">
         <Navbar />
@@ -37,8 +65,21 @@ export default function App() {
           <button className="nav--btn" onClick={addRow}>Add Row</button>
           <button className="nav--btn" onClick={addColumn}> Add Column</button>
           <input className="color--picker" type="color" value="#FF0000"/>
+          <div className="display_row_col">
+            <p>Rows: {grid.rows}</p>
+            <p>Cols: {grid.cols}</p>
+          </div>
         </div>
-      </div>
+            
+          <table id="my-table">
+            {/* <AddRow /> */}
+            {/* Table.jsx could be the parent of TableRow.jsx (presentational component)
+              TableRow.jsx could be the parent of TableCell.jsx (presentational component) */}
+
+
+          </table>
+        </div>
+      
     );
 }
 
