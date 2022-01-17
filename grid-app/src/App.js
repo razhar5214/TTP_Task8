5e8002f76cf1d05b52c01b273e61bb5ead40bce1
Awais <razhar5214@bths.edu>
2022-01-17 06:52:50 -0500

    set up navbar and buttons

--- grid-app/src/App.js
+++ grid-app/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from "react"
+import ReactDOM from "react"
 import './App.css';
+import { useState } from "react"
+import Navbar from "./Components/Navbar"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+
+  const [grid, setGrid] = useState(
+    {
+      rows:0,
+      cols:0
+    }
   );
+
+  function addRow(){
+    //console.log('add row');
+    setGrid(prevGrid => ({
+      ...prevGrid,
+      rows: prevGrid.rows+1
+    }))
+  }
+
+  function addColumn(){
+    //console.log('add col');
+    setGrid(prevGrid => ({
+      ...prevGrid,
+      cols: prevGrid.cols+1
+    }))
+  }
+
+
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="btn--container">
+          <button className="nav--btn" onClick={addRow}>Add Row</button>
+          <button className="nav--btn" onClick={addColumn}> Add Column</button>
+          <input className="color--picker" type="color" value="#FF0000"/>
+        </div>
+      </div>
+    );
 }
 
-export default App;
+
