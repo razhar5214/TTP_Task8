fb2e13f88b2231e59ccd013e85c8f3a723738540
Shakhram <shakag6@gmail.com>
2022-01-17 19:21:32 -0500

    turned App into class component. closes #3

--- grid-app/src/App.js
+++ grid-app/src/App.js
@@ -3,84 +3,83 @@ import ReactDOM from "react"
 import './App.css';
 import { useState } from "react"
 import Navbar from "./Components/Navbar"
+import Table from "./Components/Table"
 
-export default function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const [grid, setGrid] = useState(
-    {
-      rows:0,
-      cols:0
-    }
-  );
-   
-  function addRow(){
-    //console.log('add row');
-    setGrid(prevGrid => ({
-      ...prevGrid,
-      rows: prevGrid.rows+1
-    }))
-    appendRow(grid.rows);
+    this.state = {
+      color: "transparent",
+      row: 0,
+      column: 0
+    };
   }
 
-  function addColumn(){
-    //console.log('add col');
-    setGrid(prevGrid => ({
-      ...prevGrid,
-      cols: prevGrid.cols+1
-    }))
-    appendColumn();
+  addRow = () => {
+    this.setState({ row: this.state.row + 1 });
+    this.appendRow(this.row);
   }
-  function appendRow(rows) {
+
+  addColumn = () => {
+    this.setState({ column: this.state.column + 1 });
+    this.appendColumn();
+  }
+
+  appendRow = (rows) => {
     var tbl = document.getElementById('my-table'), // table reference
-        row = tbl.insertRow(tbl.rows.length),      // append table row
-        i;
+      row = tbl.insertRow(tbl.rows.length),      // append table row
+      i;
     // insert table cells to the new row
     for (i = 0; i < tbl.rows[0].cells.length; i++) {
-        createCell(row.insertCell(i));
+      this.createCell(row.insertCell(i));
     }
   }
-  function appendColumn() {
+
+  appendColumn = () => {
     var tbl = document.getElementById('my-table'), // table reference
-        i;
+      i;
     // open loop for each row and append cell
     for (i = 0; i < tbl.rows.length; i++) {
-        createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length));
+      this.createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length));
     }
-}
- 
-// create DIV element and append to the table cell
-  function createCell(cell, style) {
-      var div = document.createElement('div'); // create DIV element
-      div.setAttribute('class', style);        // set DIV class attribute
-      div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
-      cell.appendChild(div);                   // append DIV to the table cell
   }
 
+  // create DIV element and append to the table cell
+  createCell = (cell, style) => {
+    var div = document.createElement('div'); // create DIV element
+    div.setAttribute('class', style);        // set DIV class attribute
+    div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
+    cell.appendChild(div);                   // append DIV to the table cell
+  }
 
- 
+  render() {
     return (
       <div className="App">
         <Navbar />
         <div className="btn--container">
-          <button className="nav--btn" onClick={addRow}>Add Row</button>
-          <button className="nav--btn" onClick={addColumn}> Add Column</button>
-          <input className="color--picker" type="color" value="#FF0000"/>
+          <button className="nav--btn" onClick={this.addRow}>Add Row</button>
+          <button className="nav--btn" onClick={this.addColumn}> Add Column</button>
+          <input className="color--picker" type="color" value="#FF0000" />
           <div className="display_row_col">
-            <p>Rows: {grid.rows}</p>
-            <p>Cols: {grid.cols}</p>
+            <p>Rows: {this.state.row}</p>
+            <p>Cols: {this.state.column}</p>
           </div>
         </div>
-            
-          <table id="my-table">
-            {/* <AddRow /> */}
-            {/* Table.jsx could be the parent of TableRow.jsx (presentational component)
+
+        <table id="my-table">
+          {/* <AddRow /> */}
+          {/* Table.jsx could be the parent of TableRow.jsx (presentational component)
               TableRow.jsx could be the parent of TableCell.jsx (presentational component) */}
-
-
-          </table>
-        </div>
-      
-    );
+          <Table
+            color={this.state.color}
+            row={this.state.row}
+            column={this.state.column}
+          />
+        </table>
+      </div>
+    )
+  }
 }
 
-
+export default App;
