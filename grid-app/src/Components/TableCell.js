d65272de1496ee25fd46f36b4310940f9ab60e6e
Shakhram <shakag6@gmail.com>
2022-01-18 14:10:07 -0500

    Removed commented code

--- grid-app/src/Components/TableCell.js
+++ grid-app/src/Components/TableCell.js
@@ -2,24 +2,6 @@ import React from "react";
 import { ReactDOM } from "react";
 import { useState } from "react";
 
-// export default class TableCell extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             color: ""
-//         }
-//     }
-
-//     handleClick() {
-//         // this.setState({ color: this.state.color })
-//     }
-
-//     render() {
-//         return (
-//             <div className="cell" style={{ backgroundColor: this.state.color }} onClick={this.handleClick}></div >
-//         )
-//     }
-// }
 export default function TableCell(props) {
     const [color, setColor] = useState("transparent")
 
