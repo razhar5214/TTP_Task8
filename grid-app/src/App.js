c5886e120161df3b032287973b11767d62f088cc
Awais <razhar5214@bths.edu>
2022-01-18 19:48:37 -0500

    post work

--- grid-app/src/App.js
+++ grid-app/src/App.js
@@ -30,33 +30,37 @@ export default function App() {
     }))
     appendColumn();
   }
+
+  // append row to the HTML table
   function appendRow(rows) {
     var tbl = document.getElementById('my-table'), // table reference
         row = tbl.insertRow(tbl.rows.length),      // append table row
         i;
     // insert table cells to the new row
     for (i = 0; i < tbl.rows[0].cells.length; i++) {
-        createCell(row.insertCell(i));
+        createCell(row.insertCell(i), i, 'row');
     }
   }
-  function appendColumn() {
-    var tbl = document.getElementById('my-table'), // table reference
-        i;
-    // open loop for each row and append cell
-    for (i = 0; i < tbl.rows.length; i++) {
-        createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length));
-    }
-}
- 
-// create DIV element and append to the table cell
-  function createCell(cell, style) {
-      var div = document.createElement('div'); // create DIV element
+
+    // create DIV element and append to the table cell
+    function createCell(cell, text, style) {
+      var div = document.createElement('div'), // create DIV element
+          txt = document.createTextNode(text); // create text node
+      div.appendChild(txt);                    // append text node to the DIV
       div.setAttribute('class', style);        // set DIV class attribute
       div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
       cell.appendChild(div);                   // append DIV to the table cell
+    }
+
+  // append column to the HTML table
+function appendColumn() {
+  var tbl = document.getElementById('my-table'), // table reference
+      i;
+  // open loop for each row and append cell
+  for (i = 0; i < tbl.rows.length; i++) {
+      createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), i, 'col');
   }
-
-
+}
  
     return (
       <div className="App">
